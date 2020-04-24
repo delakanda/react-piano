f14ebcb1a4e12b17e302423217a51d896f1e6d8f
Delali Kanda <delali@spryonline.com>
2020-04-24 19:40:01 +0000

    added keyboard test

--- src/components/keyboard/Keyboard.test.tsx
+++ src/components/keyboard/Keyboard.test.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, cleanup, fireEvent, waitForDomChange } from '@testing-library/react';
 import Keyboard from './Keyboard';
+import { KEYBOARD_KEYS } from '../../constants/Piano';
 
-test('renders keyboard successfully', () => {
-  // const { getByText } = render(<Keyboard />);
-  // const linkElement = getByText(/learn react/i);
-  // expect(linkElement).toBeInTheDocument();
+// const VALID_KEY = KEYBOARD_KEYS[0].key;
+
+afterEach(cleanup);
+
+test('Count number of displayed keyboard keys', () => {
+  const {keyboardContainer} = setup();
+
+  expect(keyboardContainer.children.length).toBe(KEYBOARD_KEYS.length);
 });
+
+test('Count number of displayed keyboard keys', () => {
+  const {keyboardContainer} = setup();
+
+  expect(keyboardContainer.children.length).toBe(KEYBOARD_KEYS.length);
+});
+
+test('Keyboard key press should highlight key', () => {
+  const {keyboardContainer} = setup();
+  
+  const firstKey = keyboardContainer.firstChild;
+  expect(firstKey).not.toBeNull();
+
+  if(firstKey) {
+    fireEvent.click(firstKey);
+    waitForDomChange();
+    expect(firstKey.className).toBe('key highlight');
+  }
+});
+
+// Functions
+function setup() {
+  const utils = render(<Keyboard handleKeyboardKeyPress={() => {}} inputActiveKey={''} />)
+  const keyboardContainer = utils.getByTestId('keyboard');
+  return {
+    keyboardContainer,
+    ...utils,
+  }
+}
